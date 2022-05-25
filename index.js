e55c4962d9eb6dc01de9154a5a6b7a01ddb28cc1
Bobby Hinton <bob.g.hint@gmail.com>
2022-05-25 14:33:24 -0500

    commit tree c70a3bb009995819ae5782c5d49de378c2905077

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(array) {
+   let obj = array.find(element => element.result === `W`); 
+   console.log(obj);
+   if(obj !== undefined){
+     return obj.year;
+   } else {
+     return obj; 
+   }
+ }
